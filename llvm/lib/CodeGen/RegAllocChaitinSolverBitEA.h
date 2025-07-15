ceff2613b22b4d0196f4c3edb016ba8ebcb07110
Alihan Turan <96257580+aturanix@users.noreply.github.com>
2025-07-15 18:47:54 +0300

    Delete llvm/lib/CodeGen/RegAllocChaitinSolverBitEA.h

--- llvm/lib/CodeGen/RegAllocChaitinSolverBitEA.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "RegAllocChaitinRegisters.h"
-
-namespace alihan {
-class InterferenceGraph;
-[[nodiscard]] auto solveBitEA(const InterferenceGraph &graph,
-                               unsigned numberOfColors) -> SolutionMap;
-} // namespace alihan
